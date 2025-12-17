8b2e5b6aea4a4312b786f0ae2e2cc4c07f1fbee2
stevecurthoys-sudo <stevecurthoys@gmail.com>
2025-12-17 17:42:04 +0000

    add cors support to backend

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const app = express();
+const cors = reqire('cors');
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(cors());
 
 // Database connection
 const pool = new Pool({
